237a8f27ebccead4863b6a491d31e718fb80afa0
Aurel Mirashi <65659646+Aurel88-00@users.noreply.github.com>
2025-01-20 14:54:10 +0100

    Initial commit

---
+++ lib/bg-image.ts
@@ -0,0 +1,5 @@
+export const bgImage = {
+  opacity: 0.4,
+  image:
+    "https://images.unsplash.com/photo-1545324418-cc1a3fa10c00?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+};
